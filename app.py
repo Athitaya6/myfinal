abeee468866ad19b3289708dedef48b44e33c243
Athitaya6 <athitayakakandee@gmail.com>
2025-08-03 10:39:48 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+final-exam-web-app/
+├── web/                           
+│   ├── app.py                  
+│   ├── requirements.txt         
+│   ├── Dockerfile                
+│   ├── static/                   
+│   │   └── style.css            
+│   └── templates/             
+│       ├── index.html            
+│       ├── about.html           
+│       ├── myresearch.html      
+│       └── reference.html        
+│
+├── db/
+│   └── init.sql                 
+│
+├── docker-compose.yml            
+├── README.md                    
+└── .gitignore                    
